eabc6e26efe3dcae2c11aa59d9cba15cde3d0989
nsbcodes <299999802+nsbcodes@users.noreply.github.com>
2023-07-10 19:58:07 -0500

    Host on the whole local network

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
-import react from "@vitejs/plugin-react-swc"
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react()
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
-    },
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	server: {
+		host: true,
+	},
 })
