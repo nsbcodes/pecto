e72accf491e2fb428fd7cd7a20ad44841a2e37a0
nsbcodes <299999802+nsbcodes@users.noreply.github.com>
2023-08-20 15:37:22 -0500

    specify the manifest since vite-pwa overwrites it

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,31 @@ import terser from '@rollup/plugin-terser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), VitePWA({ injectRegister: 'auto', registerType: 'autoUpdate' })],
+	plugins: [
+		react(),
+		VitePWA({
+			injectRegister: 'auto',
+			registerType: 'autoUpdate',
+			manifest: {
+				short_name: 'Pecto',
+				name: 'Pecto - Procedural Studying Tool',
+				icons: [
+					{ src: '/icon-192.png', type: 'image/png', sizes: '192x192' },
+					{ src: '/icon-512.png', type: 'image/png', sizes: '512x512' },
+					{
+						src: '/maskable_icon.png',
+						type: 'image/png',
+						sizes: '1024x1024',
+						purpose: 'maskable',
+					},
+				],
+				start_url: '.',
+				display: 'standalone',
+				theme_color: '#ffffff',
+				background_color: '#ffffff',
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
