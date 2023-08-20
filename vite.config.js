9b059d56d866f08087eb26578ea55214feb9337f
nsbcodes <299999802+nsbcodes@users.noreply.github.com>
2023-08-20 15:32:24 -0500

    good

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import terser from '@rollup/plugin-terser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), VitePWA({ injectRegister: 'auto' })],
+	plugins: [react(), VitePWA({ injectRegister: 'auto', registerType: 'autoUpdate' })],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
